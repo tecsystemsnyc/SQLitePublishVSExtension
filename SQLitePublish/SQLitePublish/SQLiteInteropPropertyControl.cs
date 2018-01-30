af8f0cdbc647241035d7da3b6444517fb287f92c
Gregory Hanson <ghanson@tec-system.com>
2018-01-30 15:40:08 -0500

    It builds

---
+++ SQLitePublish/SQLitePublish/SQLiteInteropPropertyControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Editors.PropertyPages;
+
+namespace SQLitePublish
+{
+    public partial class SQLiteInteropPropertyControl : PropPageUserControlBase
+    {
+        public SQLiteInteropPropertyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
